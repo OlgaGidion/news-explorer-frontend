298f8f4ab99a9b91a3938d052c5a9a9d4c6b2f26
Olga Gidion <olga.gidion@gmail.com>
2020-12-17 21:39:45 +0100

    Working on header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       <header className="header header_color_light">
-        <h1 className="header__name header__name_color_light">NewsExplorer</h1>
+        <h1 className="header__name">NewsExplorer</h1>
         <nav className="header__nav">
           <button className="button header__nav-button header__nav-button_color_light header__nav-button_selected-light">Главная</button>
           <button className="button button-rounded button-rounded_type_wired-light button-rounded_small">Авторизоваться</button>
