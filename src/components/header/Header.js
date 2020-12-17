ba7cb7f7bd67fb2f23dcdb05af2363363ca79732
Olga Gidion <olga.gidion@gmail.com>
2020-12-17 11:21:00 +0100

    Working on header dark

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <>
       <header className="header">
-        <h1 className="header__name">NewsExplorer</h1>
+        <h1 className="header__name header__name_color_dark">NewsExplorer</h1>
         <nav className="header__nav">
-          <button className="button header__nav-button header__nav-button_selected">Главная</button>
-          <button className="button header__nav-button">Сохранённые статьи</button>
+          <button className="button header__nav-button header__nav-button_color_dark header__nav-button_selected-dark">Главная</button>
+          <button className="button header__nav-button header__nav-button_color_dark">Сохранённые статьи</button>
           <button className="button button-rounded button-rounded_type_wired-dark button-rounded_small">Olga <img className="button-rounded__icon-right" src={logoutDarkImage} alt="icon" /></button>
         </nav>
       </header>
