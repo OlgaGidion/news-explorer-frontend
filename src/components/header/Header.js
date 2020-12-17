416b33f8b1a491620d4c1be73532d251f253efbf
Olga Gidion <olga.gidion@gmail.com>
2020-12-17 11:09:51 +0100

    Working on header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
       <header className="header">
         <h1 className="header__name">NewsExplorer</h1>
         <nav className="header__nav">
-          <button className="header__nav-button header__nav-button_selected">Главная</button>
-          <button className="header__nav-button">Сохранённые статьи</button>
-          <button className="button button_type_wired-dark button_small">Olga <img className="button__icon-right" src={logoutDarkImage} alt="icon" /></button>
+          <button className="button header__nav-button header__nav-button_selected">Главная</button>
+          <button className="button header__nav-button">Сохранённые статьи</button>
+          <button className="button button-rounded button-rounded_type_wired-dark button-rounded_small">Olga <img className="button-rounded__icon-right" src={logoutDarkImage} alt="icon" /></button>
         </nav>
       </header>
     </>
--- src/testers/ButtonsTester.js
+++ src/testers/ButtonsTester.js
@@ -4,28 +4,28 @@ const ButtonsTester = () => {
   return (
     <>
       <div>
-        <button className="button button_type_solid-blue">I'm blue round button</button>
-        <button className="button button_type_solid-blue button_small">Small</button>
+        <button className="button button-rounded button-rounded_type_solid-blue">I'm blue round button</button>
+        <button className="button button-rounded button-rounded_type_solid-blue button-rounded_small">Small</button>
       </div>
 
       <div style={{background: 'gray', padding: '20px 0', margin: '20px 0'}}>
-        <button className="button button_type_solid-white">I'm white round button</button>
-        <button className="button button_type_solid-white button_small">Small</button>
-        <button className="button button_type_disabled">Small disabled</button>
+        <button className="button button-rounded button-rounded_type_solid-white">I'm white round button</button>
+        <button className="button button-rounded button-rounded_type_solid-white button-rounded_small">Small</button>
+        <button className="button button-rounded button-rounded_type_disabled">Small disabled</button>
       </div>
 
       <div style={{background: 'black', margin: '20px 0'}}>
-        <button className="button button_type_wired-light">I'm wired round button</button>
-        <button className="button button_type_wired-light button_small">I'm wired round button</button>
+        <button className="button button-rounded button-rounded_type_wired-light">I'm wired round button</button>
+        <button className="button button-rounded button-rounded_type_wired-light button-rounded_small">I'm wired round button</button>
       </div>
 
       <div style={{background: 'white', margin: '20px 0'}}>
-        <button className="button button_type_wired-dark">I'm wired round button</button>
-        <button className="button button_type_wired-dark button_small">I'm wired round button <img className="button__icon-right" src={logoutDarkImage} alt="icon" /></button>
+        <button className="button button-rounded button-rounded_type_wired-dark">I'm wired round button</button>
+        <button className="button button-rounded button-rounded_type_wired-dark button-rounded_small">I'm wired round button <img className="button-rounded__icon-right" src={logoutDarkImage} alt="icon" /></button>
       </div>
 
       <div style={{fontSize: '30px', fontFamily: 'sans-serif'}}>
-        This text contains <button className="button-text">text button</button> inside
+        This text contains <button className="button button-text">text button</button> inside
 
       </div>
     </>
