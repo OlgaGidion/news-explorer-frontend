c29bbf1e20fdff87ed30c89680bb2fe90fb38151
Olga Gidion <olga.gidion@gmail.com>
2020-12-18 11:11:26 +0100

    Using airbnb eslint

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,10 @@
 import MainPage from '../MainPage/MainPage';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="app">
-      <MainPage />
-    </div>
-  );
-};
+const App = () => (
+  <div className="app">
+    <MainPage />
+  </div>
+);
 
 export default App;
