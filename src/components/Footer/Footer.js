209465b568877f5955fafd80ca9c8d05518bfcb0
Olga Gidion <olga.gidion@gmail.com>
2020-12-19 20:58:34 +0100

    Working on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,21 +7,21 @@ const Footer = () => (
     <div className="footer__info">&copy; 2020 Supersite, Powered by News API</div>
     <ul className="footer__text-list">
       <li className="footer__item">
-        <a className="footer__link" href="#">Главная</a>
+        <a className="footer__link" href="/">Главная</a>
       </li>
       <li className="footer__item">
-        <a className="footer__link" href="#">Яндекс.Практикум</a>
+        <a className="footer__link" href="https://praktikum.yandex.ru" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
       </li>
     </ul>
     <ul className="footer__icon-list">
       <li className="footer__item">
-        <a href="#">
-          <img className="footer__icon" src={githubImage} alt="github" />
+        <a href="https://github.com/OlgaGidion" target="_blank" rel="noreferrer">
+          <img className="footer__icon" src={githubImage} alt="GitHub" />
         </a>
       </li>
       <li className="footer__item">
-        <a href="#">
-          <img className="footer__icon" src={facebookImage} alt="facebook" />
+        <a href="https://www.facebook.com/olga.gidion" target="_blank" rel="noreferrer">
+          <img className="footer__icon" src={facebookImage} alt="Facebook" />
         </a>
       </li>
     </ul>
