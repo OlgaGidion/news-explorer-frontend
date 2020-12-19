09b6a07863a4680a193ccd5620f7c29f41926f34
Olga Gidion <olga.gidion@gmail.com>
2020-12-19 13:40:39 +0100

    Working on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,20 +5,22 @@ import facebookImage from '../../images/facebook.svg';
 const Footer = () => (
   <footer className="footer">
     <div className="footer__info">&copy; 2020 Supersite, Powered by News API</div>
-    <ul className="footer__list">
+    <ul className="footer__text-list">
       <li className="footer__item">
         <a className="footer__link" href="#">Главная</a>
       </li>
       <li className="footer__item">
         <a className="footer__link" href="#">Яндекс.Практикум</a>
       </li>
+    </ul>
+    <ul className="footer__icon-list">
       <li className="footer__item">
-        <a className="footer__link" href="#">
+        <a href="#">
           <img className="footer__icon" src={githubImage} alt="github" />
         </a>
       </li>
       <li className="footer__item">
-        <a className="footer__link" href="#">
+        <a href="#">
           <img className="footer__icon" src={facebookImage} alt="facebook" />
         </a>
       </li>
