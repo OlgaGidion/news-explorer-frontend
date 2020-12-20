57877c7e7aec5fa63785cfff28377d7fd5cbc6d3
Olga Gidion <olga.gidion@gmail.com>
2020-12-20 15:26:56 +0100

    Working on search form

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,6 +1,7 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Navigation from '../Navigation/Navigation';
+import SearchForm from '../SearchForm/SearchForm';
 import './MainPage.css';
 
 const MainPage = () => (
@@ -12,6 +13,7 @@ const MainPage = () => (
       <div className="main-page__content">
         <h1 className="main-page__headline">Что творится в мире?</h1>
         <p className="main-page__subline">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+        <SearchForm />
       </div>
     </div>
     <Footer />
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,10 @@
+import './SearchForm.css';
+
+const SearchForm = () => (
+  <form className="form">
+    <input className="form__input" />
+    <button />
+  </form>
+);
+
+export default SearchForm;
