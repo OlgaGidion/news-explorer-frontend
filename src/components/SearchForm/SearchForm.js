c42d651c369044df06d558d64cf4d9a92dd4463f
Olga Gidion <olga.gidion@gmail.com>
2020-12-20 16:49:12 +0100

    Working on search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './SearchForm.css';
 
-const SearchForm = ({ placeholder, buttonText }) => (
-  <form className="form">
-    <input className="form__input" placeholder={placeholder} />
-    <button className="button button-rounded button-rounded_type_solid-blue">{buttonText}</button>
-  </form>
-);
+const SearchForm = ({ placeholder, buttonText }) => {
+  const [text, setText] = React.useState('');
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <form className="form">
+      <input className="form__input" placeholder={placeholder} name="text" value={text} onChange={handleInputChange} />
+      <button className="button button-rounded button-rounded_type_solid-blue">{buttonText}</button>
+    </form>
+  );
+};
 
 export default SearchForm;
