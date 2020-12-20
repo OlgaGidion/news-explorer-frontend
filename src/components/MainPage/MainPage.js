de10175d8b02d6b422713953b50748a8d9ceb8fe
Olga Gidion <olga.gidion@gmail.com>
2020-12-20 14:33:36 +0100

    Working on main page

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,12 +5,14 @@ import './MainPage.css';
 
 const MainPage = () => (
   <main className="main-page">
-    <div className="main-page__header">
+    <div className="main-page__image-container">
       <Header isDark={false}>
         <Navigation isDark={false} />
       </Header>
-      <h1 className="main-page__headline">Что творится в мире?</h1>
-      <p className="main-page__subline">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+      <div className="main-page__content">
+        <h1 className="main-page__headline">Что творится в мире?</h1>
+        <p className="main-page__subline">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+      </div>
     </div>
     <Footer />
   </main>
