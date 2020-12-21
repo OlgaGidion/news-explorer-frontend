1ed9b5394f483cd89c7c6d211e13d34eab96e0c3
Olga Gidion <olga.gidion@gmail.com>
2020-12-21 18:28:13 +0100

    Working on news card list

---
+++ src/components/NewsCardList/NewsCardList.js
@@ -0,0 +1,20 @@
+import './NewsCardList.css';
+
+const NewsCardList = () => (
+  <section className="news-cards">
+    <ul className="news-cards__list">
+      <li className="news-cards__item">
+      </li>
+      <li className="news-cards__item">
+      </li>
+      <li className="news-cards__item">
+      </li>
+      <li className="news-cards__item">
+      </li>
+      <li className="news-cards__item">
+      </li>
+    </ul>
+  </section>
+);
+
+export default NewsCardList;
--- src/components/SavedNewsPage/SavedNewsPage.js
+++ src/components/SavedNewsPage/SavedNewsPage.js
@@ -1,5 +1,7 @@
 import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
+import NewsCardList from '../NewsCardList/NewsCardList';
+import Footer from '../Footer/Footer';
 import './SavedNewsPage.css';
 
 const SavedNewsPage = () => (
@@ -8,10 +10,12 @@ const SavedNewsPage = () => (
       <Navigation isDark={true} />
     </Header>
     <div className="saved-news-page__content">
-      <p className="saved-news-page__text">Сохранённые статьи</p>
+      <h3 className="saved-news-page__text">Сохранённые статьи</h3>
       <h2 className="saved-news-page__headline">Грета, у вас 5 сохранённых статей</h2>
       <p className="saved-news-page__subline">По ключевым словам: <span className="saved-news-page__span-accent">Природа, Тайга</span> и <span className="saved-news-page__span-accent">2-м другим</span></p>
     </div>
+    <NewsCardList />
+    <Footer />
   </main>
 );
 
