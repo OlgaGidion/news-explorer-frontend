bdcf20426eaa2c619f1d7178ae57de945ca95fa1
Olga Gidion <olga.gidion@gmail.com>
2020-12-23 22:32:25 +0100

    Working on news card

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,16 @@
 import './NewsCard.css';
+import deleteImage from '../../images/delete.svg';
 
 const NewsCard = ({ headline, text, source, date, imageUrl, category }) => (
   <div className="news-card">
     <div className="news-card__image-container">
       <img className="news-card__image" src={imageUrl} alt={headline} />
-      <div className="news-card__category">{category}</div>
+      <div className="news-card__category">
+        <p className="news-card__category-text">{category}</p>
+      </div>
+      <button className="button button-icon news-card__button">
+        <img className="button-icon__image" src={deleteImage} alt="Удалить" />
+      </button>
     </div>
     <div className="news-card__text-container">
       <p className="news-card__date">{date}</p>
