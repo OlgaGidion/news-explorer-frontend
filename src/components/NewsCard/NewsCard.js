a1e8d1fa7dba59ef285599ae74b498f0c1260dd0
Olga Gidion <olga.gidion@gmail.com>
2020-12-23 09:52:04 +0100

    Working on news card

---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,20 @@
+import './NewsCard.css';
+
+const NewsCard = ({ imageUrl }) => (
+  <div className="news-card">
+    <img className="news-card__image" src={imageUrl} alt="Национальное достояние – парки" />
+    <div className="news-card__text-container">
+      <p className="news-card__date">2 августа, 2019</p>
+      <h3 className="news-card__headline">Национальное достояние – парки</h3>
+      <p className="news-card__text">
+        В 2016 году Америка отмечала важный
+        юбилей: сто лет назад здесь начала
+        складываться система национальных
+        парков – охраняемых территорий, где
+        и сегодня каждый может приобщиться к природе.</p>
+      <p className="news-card__source">Дзен</p>
+    </div>
+  </div>
+);
+
+export default NewsCard;
--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,17 +1,23 @@
+import NewsCard from '../NewsCard/NewsCard';
 import './NewsCardList.css';
 
 const NewsCardList = () => (
   <section className="news-cards">
     <ul className="news-cards__list">
       <li className="news-cards__item">
+        <NewsCard imageUrl="https://images.unsplash.com/photo-1605718666382-4eb9bd7e1961?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" />
       </li>
       <li className="news-cards__item">
+        <NewsCard imageUrl="https://images.unsplash.com/photo-1605559911160-a3d95d213904?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=954&q=80" />
       </li>
       <li className="news-cards__item">
+        <NewsCard imageUrl="https://images.unsplash.com/photo-1605559911160-a3d95d213904?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=954&q=80" />
       </li>
       <li className="news-cards__item">
+        <NewsCard imageUrl="https://images.unsplash.com/photo-1605718666382-4eb9bd7e1961?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" />
       </li>
       <li className="news-cards__item">
+        <NewsCard imageUrl="https://images.unsplash.com/photo-1605559911160-a3d95d213904?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=954&q=80" />
       </li>
     </ul>
   </section>
