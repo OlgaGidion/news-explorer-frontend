ac9170cde1c09e17640048bcc4aac20cc9cc2224
Olga Gidion <olga.gidion@gmail.com>
2020-12-23 12:15:59 +0100

    Working on news card

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,20 +1,21 @@
 import './NewsCard.css';
 
-const NewsCard = ({ imageUrl }) => (
+const NewsCard = ({
+  headline,
+  text,
+  source,
+  date,
+  imageUrl,
+}) => (
   <div className="news-card">
     <div className="news-card__image-container">
-      <img className="news-card__image" src={imageUrl} alt="Национальное достояние – парки" />
+      <img className="news-card__image" src={imageUrl} alt={headline} />
     </div>
     <div className="news-card__text-container">
-      <p className="news-card__date">2 августа, 2019</p>
-      <h3 className="news-card__headline">Национальное достояние – парки</h3>
-      <p className="news-card__text">
-        В 2016 году Америка отмечала важный
-        юбилей: сто лет назад здесь начала
-        складываться система национальных
-        парков – охраняемых территорий, где
-        и сегодня каждый может приобщиться к природе.</p>
-      <p className="news-card__source">Дзен</p>
+      <p className="news-card__date">{date}</p>
+      <h3 className="news-card__headline">{headline}</h3>
+      <p className="news-card__text">{text}</p>
+      <p className="news-card__source">{source}</p>
     </div>
   </div>
 );
