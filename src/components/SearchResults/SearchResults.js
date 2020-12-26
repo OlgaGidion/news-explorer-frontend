034b92cd3375f54ca4255c423c157fcdf659ee95
Olga Gidion <olga.gidion@gmail.com>
2020-12-26 13:51:12 +0100

    Working on search results

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,11 +1,8 @@
 import NewsCardResult from '../NewsCardResult/NewsCardResult';
-import SearchPreloader from '../SearchPreloader/SearchPreloader';
 import './SearchResults.css';
 
 const SearchResults = () => (
   <section className="search-results">
-    <SearchPreloader />
-
     <h2 className="search-results__headline">Результаты поиска</h2>
     <ul className="search-results__list">
       <li className="search-results__item">
