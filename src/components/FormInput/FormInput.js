71238f1baaa36615959c01c32c60d227ed56b8ea
Olga Gidion <olga.gidion@gmail.com>
2020-12-28 21:33:18 +0100

    Working on input

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,27 +1,36 @@
+import React from 'react';
 import './FormInput.css';
 
-const FormInput = ({ label, name, type, placeholder, required, minLength, maxLength }) => (
-  <>
-    <label className="form-input__label" for={name}>
-      {label}
-    </label>
+const FormInput = ({ label, name, type, placeholder, required, minLength, maxLength }) => {
+  const [value, setValue] = React.useState('');
 
-    <input
-      className="form-input__input-text"
-      name={name}
-      type={type}
-      // value={email}
-      placeholder={placeholder}
-      required={required}
-      minLength={minLength}
-      maxLength={maxLength}
-      // onChange={handleEmailChange}
-    />
+  const handleChange = (e) => {
+    const text = e.target.value;
+    setValue(text);
+  };
 
-    <span className="form-input__input-error form-input__input-error_hidden">
-      Error
-    </span>
-  </>
-);
+  return (
+    <>
+      <label className="form-input__label" htmlFor={name}>
+        {label}
+      </label>
+
+      <input
+        className="form-input__input-text"
+        name={name}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        required={required}
+        minLength={minLength}
+        maxLength={maxLength}
+        onChange={handleChange} />
+
+      <span className="form-input__input-error form-input__input-error_hidden">
+        Error
+      </span>
+    </>
+  );
+};
 
 export default FormInput;
