45e484c7c62b2cb24236413b85fcbe71b5d90e62
Olga Gidion <olga.gidion@gmail.com>
2021-01-02 10:14:05 +0100

    Working on routing in navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,14 +3,14 @@ import logoutLightImage from '../../images/logout-light.svg';
 import './Navigation.css';
 
 const Navigation = ({ isDark }) => {
-  const navButtonMod = isDark ? 'navigation-button_color_dark' : 'navigation-button_color_light';
+  const navLinkMod = isDark ? 'navigation-link_color_dark' : 'navigation-link_color_light';
   const wiredButtonType = isDark ? 'dark' : 'light';
 
   return (
     <nav className="navigation">
 
-      <button className={`navigation-button ${navButtonMod} navigation-button_selected`}>Главная</button>
-      <button className={`navigation-button ${navButtonMod}`}>Сохранённые статьи</button>
+      <a className={`navigation-link ${navLinkMod} navigation-link_selected`} href="/">Главная</a>
+      <a className={`navigation-link ${navLinkMod}`} href="/saved-news">Сохранённые статьи</a>
 
       <ButtonWired type={wiredButtonType} text="Грета" image={logoutLightImage} imageAlt="Выйти" />
 
