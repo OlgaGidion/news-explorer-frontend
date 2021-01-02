9d45d7210977052eb0e9f770be34a9ae0149eb9e
Olga Gidion <olga.gidion@gmail.com>
2021-01-02 09:56:08 +0100

    Working on routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MainPage from '../MainPage/MainPage';
 import SavedNewsPage from '../SavedNewsPage/SavedNewsPage';
 import './App.css';
 
 const App = () => (
-  <div className="app">
-    <SavedNewsPage />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+
+        <Route path="/saved-news" >
+          <SavedNewsPage />
+        </Route>
+
+        <Route exact path="/" >
+          <MainPage />
+        </Route>
+
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
