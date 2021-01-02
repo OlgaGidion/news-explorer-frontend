bef96b940c8e620869c38a1fc0e63ee7360043a5
Olga Gidion <olga.gidion@gmail.com>
2021-01-02 09:39:43 +0100

    Working on saved news header

---
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -0,0 +1,31 @@
+import './SavedNewsHeader.css';
+
+const SavedNewsHeader = ({ userName, articlesCount, keywords }) => {
+  const headlineText = articlesCount > 0
+    ? `${userName}, у вас ${articlesCount} сохранённых статей`
+    : `${userName}, у вас нет сохранённых статей`;
+
+  const maxKeywords = 2;
+  const tailLength = keywords.length - maxKeywords;
+  const keywordHighlights = tailLength > 1
+    ? [...keywords.slice(0, maxKeywords), `${tailLength}-м другим`]
+    : keywords;
+
+  const keywordElements = keywordHighlights.map((highlight, i) => (
+    <>
+      <span key={i} className="saved-news-header__highlight">{highlight}</span>
+      {i < keywordHighlights.length - 2 && <>, </>}
+      {i === keywordHighlights.length - 2 && <> и </>}
+    </>
+  ));
+
+  return (
+    <div className="saved-news-header">
+        <h3 className="saved-news-header__text">Сохранённые статьи</h3>
+        <h2 className="saved-news-header__headline">{headlineText}</h2>
+        <p className="saved-news-header__subline">По ключевым словам: {keywordElements}</p>
+    </div>
+  );
+};
+
+export default SavedNewsHeader;
--- src/components/SavedNewsPage/SavedNewsPage.js
+++ src/components/SavedNewsPage/SavedNewsPage.js
@@ -1,19 +1,20 @@
 import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
+import SavedNewsHeader from '../SavedNewsHeader/SavedNewsHeader';
 import NewsCardList from '../NewsCardList/NewsCardList';
 import Footer from '../Footer/Footer';
 import './SavedNewsPage.css';
 
 const SavedNewsPage = () => (
-  <main className="saved-news-page">
+  <main>
     <Header isDark={true}>
       <Navigation isDark={true} />
     </Header>
-    <div className="saved-news-page__content">
-      <h3 className="saved-news-page__text">Сохранённые статьи</h3>
-      <h2 className="saved-news-page__headline">Грета, у вас 5 сохранённых статей</h2>
-      <p className="saved-news-page__subline">По ключевым словам: <span className="saved-news-page__highlight">Природа, Тайга</span> и <span className="saved-news-page__highlight">2-м другим</span></p>
-    </div>
+
+    <SavedNewsHeader
+      userName="Грета"
+      articlesCount={5}
+      keywords={['Природа', 'Автомобили', 'Дети', 'Психология']} />
     <NewsCardList />
     <Footer />
   </main>
