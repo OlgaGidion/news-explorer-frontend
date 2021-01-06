06a2d215e746c44ca3cb30e59adfcf027cdca64d
Olga Gidion <olga.gidion@gmail.com>
2021-01-06 21:04:14 +0100

    Working on mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,17 +1,17 @@
 import './MobileMenu.css';
 
-const MobileMenu = ({ isOpen, children }) => {
+const MobileMenu = ({ isOpen, onClose, children }) => {
   const classList = isOpen
     ? 'mobile-menu mobile-menu_opened'
     : 'mobile-menu';
 
   return (
     <div className={classList}>
-      <div className="mobile-menu__overlay" />
+      <div className="mobile-menu__overlay" onClick={onClose} />
       <div className="mobile-menu__container">
         <header className="mobile-menu__header">
           <h1 className="mobile-menu__header-name">NewsExplorer</h1>
-          <button className="mobile-menu__close-button" type="button" />
+          <button className="mobile-menu__close-button" type="button" onClick={onClose} />
         </header>
         <div className="mobile-menu__items">
           {children}
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,19 +1,30 @@
+import React from 'react';
 import MobileMenu from '../MobileMenu/MobileMenu';
 import './Navigation.css';
 
 const Navigation = ({ isDark, children }) => {
+  const [isMobileMenuOpened, setIsMobileMenuOpened] = React.useState(false);
+
   const menuButtonMod = isDark
     ? 'navigation__menu-button_color_dark'
     : 'navigation__menu-button_color_light';
+
+  const handleMobileMenuButtonClick = () => {
+    setIsMobileMenuOpened(true);
+  };
+
+  const handleMobileMenuClose = () => {
+    setIsMobileMenuOpened(false);
+  };
 
   return (
     <nav className="navigation">
       <div className="navigation__container">
         {children}
       </div>
-      <button className={`navigation__menu-button ${menuButtonMod}`} />
+      <button className={`navigation__menu-button ${menuButtonMod}`} onClick={handleMobileMenuButtonClick} />
 
-      <MobileMenu isOpen={true}>
+      <MobileMenu isOpen={isMobileMenuOpened} onClose={handleMobileMenuClose}>
         {children}
       </MobileMenu>
 
