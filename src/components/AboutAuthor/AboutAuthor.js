c492073e11c938a57257d6e1c3ad5f57fde4abfb
Olga Gidion <olga.gidion@gmail.com>
2021-01-08 12:13:05 +0100

    Added alts

--- src/components/AboutAuthor/AboutAuthor.js
+++ src/components/AboutAuthor/AboutAuthor.js
@@ -2,7 +2,7 @@ import './AboutAuthor.css';
 
 const AboutAuthor = () => (
   <div className="about-author">
-    <img className="about-author__image" src="https://images.unsplash.com/photo-1607732994987-15674d81ba62?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" alt="#" />
+    <img className="about-author__image" src="https://images.unsplash.com/photo-1607732994987-15674d81ba62?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" alt="Фото автора" />
     <div className="about-author__text-container">
       <h2 className="about-author__heading">Об авторе</h2>
       <p className="about-author__text">
