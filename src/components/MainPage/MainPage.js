94dc4431d283836c10dfbae0a07157837e4618b6
Olga Gidion <olga.gidion@gmail.com>
2021-01-09 17:16:48 +0100

    Fixing filenames

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../Header2/Header';
+import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
 import ButtonWired from '../ButtonWired/ButtonWired';
 import SearchForm from '../SearchForm/SearchForm';
