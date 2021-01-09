e764972163c7dd9f4fe8bf3ef84e6cfa3d84b0f7
Olga Gidion <olga.gidion@gmail.com>
2021-01-09 12:44:49 +0100

    Fixes in about author

--- src/components/AboutAuthor/AboutAuthor.js
+++ src/components/AboutAuthor/AboutAuthor.js
@@ -1,20 +1,25 @@
+import AuthorPhoto from '../../images/author.jpeg';
 import './AboutAuthor.css';
 
 const AboutAuthor = () => (
   <div className="about-author">
-    <img className="about-author__image" src="https://images.unsplash.com/photo-1607732994987-15674d81ba62?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" alt="Фото автора" />
+    <img className="about-author__image" src={AuthorPhoto} alt="Фото автора" />
     <div className="about-author__text-container">
       <h2 className="about-author__heading">Об авторе</h2>
       <p className="about-author__text">
-        Это блок с описанием автора проекта.
-        Здесь следует указать, как вас зовут,
-        чем вы занимаетесь, какими технологиями
-        разработки владеете.
+        Меня зовут Ольга Гидион, я немецкая студентка Яндекс.Практикума
+        и начинающий веб-разработчик. В технологии я пришла абсолютным
+        новичком, представления о создании веб-сайтов и приложений не имела.
+        До этого я работала в сфере туризма.
       </p>
       <p className="about-author__text">
-        Также можно рассказать о процессе обучения
-        в Практикуме, чему вы тут научились, и чем
-        можете помочь потенциальным заказчикам.
+        Теперь я владею навыками создания интерактивных красивых сайтов,
+        которые работают одинаково здорово как на десктопных, так и на
+        мобильных браузерах. Я создаю семантически корректный HTML,
+        продумываю хорошо структурированный CSS по методологии БЭМ,
+        пишу код на JavaScript с использованием React. Также я имею
+        опыт с сетевым взаимодействием, серверной разработкой на
+        Node.js и базой данных MongoDB.
       </p>
     </div>
   </div>
