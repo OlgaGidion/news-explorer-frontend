f09819f5c9ba1133a1d2c5d17909a74600d84539
Olga Gidion <olga.gidion@gmail.com>
2021-01-09 11:30:39 +0100

    Some fixes

--- src/components/SavedNewsPage/SavedNewsPage.js
+++ src/components/SavedNewsPage/SavedNewsPage.js
@@ -1,3 +1,4 @@
+import { useHistory } from 'react-router-dom';
 import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
 import ButtonWired from '../ButtonWired/ButtonWired';
@@ -7,23 +8,31 @@ import Footer from '../Footer/Footer';
 import logoutImageDark from '../../images/logout-dark.svg';
 import './SavedNewsPage.css';
 
-const SavedNewsPage = () => (
-  <main>
-    <Header isDark={true} isVisibleOnMobile={true}>
-      <Navigation isDark={true}>
-        <a className="navigation__link navigation__link_color_dark" href="/">Главная</a>
-        <a className="navigation__link navigation__link_color_dark navigation__link_selected_dark" href="/saved-news">Сохранённые статьи</a>
-        <ButtonWired type="dark" text="Грета" image={logoutImageDark} imageAlt="Выйти" classMix="navigation__wired-button" />
-      </Navigation>
-    </Header>
+const SavedNewsPage = () => {
+  const history = useHistory();
 
-    <SavedNewsHeader
-      userName="Грета"
-      articlesCount={5}
-      keywords={['Природа', 'Автомобили', 'Дети', 'Психология']} />
-    <NewsCardList />
-    <Footer />
-  </main>
-);
+  const handleLogoutButtonClick = () => {
+    history.replace('/');
+  };
+
+  return (
+    <main>
+      <Header isDark={true} isVisibleOnMobile={true}>
+        <Navigation isDark={true}>
+          <a className="navigation__link navigation__link_color_dark" href="/">Главная</a>
+          <a className="navigation__link navigation__link_color_dark navigation__link_selected_dark" href="/saved-news">Сохранённые статьи</a>
+          <ButtonWired type="dark" text="Грета" image={logoutImageDark} imageAlt="Выйти" classMix="navigation__wired-button" onClick={handleLogoutButtonClick} />
+        </Navigation>
+      </Header>
+
+      <SavedNewsHeader
+        userName="Грета"
+        articlesCount={5}
+        keywords={['Природа', 'Автомобили', 'Дети', 'Психология']} />
+      <NewsCardList />
+      <Footer />
+    </main>
+  );
+};
 
 export default SavedNewsPage;
