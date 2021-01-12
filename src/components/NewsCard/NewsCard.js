267e8dfd137758510b269040807dde7585693069
Olga Gidion <olga.gidion@gmail.com>
2021-01-12 21:37:09 +0100

    Working on articles

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,25 +1,31 @@
 import './NewsCard.css';
 
-const NewsCard = ({ headline, text, source, date, category, imageUrl, children }) => (
-  <div className="news-card">
-    <div className="news-card__image-container">
-      <img className="news-card__image" src={imageUrl} alt={headline} />
-      <div className="news-card__overlay">
-        <div className="news-card__category">
-          <p className="news-card__category-text">{category}</p>
+const NewsCard = ({ headline, text, source, date, category, imageUrl, children }) => {
+  const classList = category
+    ? 'news-card__category'
+    : 'news-card__category news-card__category_hidden';
+
+  return (
+    <div className="news-card">
+      <div className="news-card__image-container">
+        <img className="news-card__image" src={imageUrl} alt={headline} />
+        <div className="news-card__overlay">
+          <div className={classList}>
+            <p className="news-card__category-text">{category}</p>
+          </div>
+          {children}
         </div>
-        {children}
+      </div>
+      <div className="news-card__text-container">
+        <p className="news-card__date">{date}</p>
+        <h3 className="news-card__headline">{headline}</h3>
+        <div className="news-card__description-container">
+          <p className="news-card__description">{text}</p>
+        </div>
+        <p className="news-card__source">{source}</p>
       </div>
     </div>
-    <div className="news-card__text-container">
-      <p className="news-card__date">{date}</p>
-      <h3 className="news-card__headline">{headline}</h3>
-      <div className="news-card__description-container">
-        <p className="news-card__description">{text}</p>
-      </div>
-      <p className="news-card__source">{source}</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default NewsCard;
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,51 +1,28 @@
+import React from 'react';
 import NewsCardResult from '../NewsCardResult/NewsCardResult';
 import ButtonSolid from '../ButtonSolid/ButtonSolid';
 import './SearchResults.css';
 
-const SearchResults = ({ articles }) => {
-  console.log(articles);
-
-  return (
-    <section className="search-results">
-      <h2 className="search-results__headline">Результаты поиска</h2>
-      <ul className="search-results__list">
-        <li className="search-results__item">
+const SearchResults = ({ articles }) => (
+  <section className="search-results">
+    <h2 className="search-results__headline">Результаты поиска</h2>
+    <ul className="search-results__list">
+      {articles.map(({ title, description, url, urlToImage, publishedAt, source }) => (
+        <li key={url} className="search-results__item">
           <NewsCardResult
-              headline="Национальное достояние – парки"
-              text="В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков – охраняемых территорий, где и сегодня каждый может приобщиться к природе."
-              source="ДЗЭН"
-              date="2 августа, 2019"
-              imageUrl="https://images.unsplash.com/photo-1605718666382-4eb9bd7e1961?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80"
-              category="Природа"
+              headline={title}
+              text={description}
+              source={source.name}
+              date={publishedAt}
+              imageUrl={urlToImage}
             />
         </li>
-        <li className="search-results__item">
-          <NewsCardResult
-              headline="Лесные огоньки: история одной фотографии"
-              text="Фотограф отвлеклась от освещения суровой политической реальности Мексики, чтобы запечатлеть ускользающую красоту одного
-              из местных чудес природы."
-              source="АФИША"
-              date="2 августа, 2019"
-              imageUrl="https://images.unsplash.com/photo-1607025892219-7092c7cd04d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
-              category="Природа"
-            />
-        </li>
-        <li className="search-results__item">
-        <NewsCardResult
-            headline="«Первозданная тайга»: новый фотопроект Игоря Шпиленка"
-            text="Знаменитый фотограф снимает первозданные леса России, чтобы рассказать о необходимости их сохранения. В этот раз он отправился в Двинско-Пинежскую тайгу, где..."
-            source="МЕДИАЗОНА"
-            date="2 августа, 2019"
-            imageUrl="https://images.unsplash.com/photo-1606066702335-7d6e0f976758?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2734&q=80"
-            category="Тайга"
-          />
-        </li>
-      </ul>
-      <div className="search-results__button-container">
-        <ButtonSolid type="white" text="Показать еще" />
-      </div>
-    </section>
-  );
-};
+      ))}
+    </ul>
+    <div className="search-results__button-container">
+      <ButtonSolid type="white" text="Показать еще" />
+    </div>
+  </section>
+);
 
 export default SearchResults;
